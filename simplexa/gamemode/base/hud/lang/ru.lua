dbcdf5c9e0e149f33d248a0a9bb917215e569da0
Artem <100233737+Spac3e@users.noreply.github.com>
2024-06-08 10:13:53 +0300

    Update ru.lua

--- simplexa/gamemode/base/hud/lang/ru.lua
+++ simplexa/gamemode/base/hud/lang/ru.lua
@@ -16,39 +16,39 @@ LANG['#Scoreboard_ForgetButton'] = 'Забыть игрока'
 LANG['#Scoreboard_PingText'] = 'Задержка'
 
 LANG['#TimeOut'] = 'Потеря соединения'
-LANG['#TimeOut_Desc'] = 'Соединение с сервером потеряно. Подключение будет автоматически восстановлено через 30 секунд. Вы можете повторить попытку подключения самостоятельно выбрав "Перезапуск" или оставить тщетные попытки.'
+LANG['#TimeOut_Desc'] = 'Соединение с сервером потеряно. Подключение будет автоматически восстановлено через 30 секунд. Вы можете повторить попытку подключения, выбрав "Перезапуск", или отказаться от попыток.'
 LANG['#Retry'] = 'Перезапуск'
 LANG['#Disconnect'] = 'Отключиться'
 LANG['#Continue'] = 'Продолжить'
 
 LANG['#RestartRequired'] = 'Требуется перезапуск'
-LANG['#RestartRequired_Desc'] = 'Ваша конфигурация игрового клиента была изменена. Требуется перезапуск игрового клиента для восстановления процедурно-созданного контента.'
+LANG['#RestartRequired_Desc'] = 'Конфигурация игрового клиента была изменена. Требуется перезапуск клиента для восстановления процедурно-созданного контента.'
 LANG['#LocalServer'] = 'Приватный сервер'
 
-LANG['#Credits_Support_Title'] = 'Созданно при моральной поддержке'
-LANG['#Credits_Support_Desc'] = 'Моей мамы, PornHub, YouTube Music и дешёвого коньяка;' 
+LANG['#Credits_Support_Title'] = 'Создано при моральной поддержке'
+LANG['#Credits_Support_Desc'] = 'Моей мамы, PornHub, YouTube Music и дешёвого коньяка;'
 
 LANG['#Credits_Authors_Title'] = 'Самураи'
 LANG['#Credits_Authors_Desc'] = 
-	'Bipolar Bear - программная архитектура, игровая механика, VFX, UI программирование, UI/UX дизайн;'..
-	'\nNellored - UI/UX и графический дизайн;'..
-	'\nCoolGoga - техническая поддержка, развёртка серверной инфраструктуры;'
+    'Bipolar Bear - программная архитектура, игровая механика, VFX, программирование UI, дизайн UI/UX;' ..
+    '\nNellored - дизайн UI/UX и графический дизайн;' ..
+    '\nCoolGoga - техническая поддержка, развёртка серверной инфраструктуры;'
 
 LANG['#Credits_Others_Title'] = 'Ронины'
 LANG['#Credits_Others_Desc'] = 
-	'GRIMM - дизайн уровня'..
-	'\nThe Epic Prophet - композиционное наполнение, саундтрек (Покойся с миром, бро);'..
-	'\nMadeInCCCP - композиционное наполнение (https://www.youtube.com/@MaDeInCCCPMUSIC);'..
-	'\nprioraror - небольшая помощь с адаптацией старых работ;'..
-	'\nDeviatt - много говорил, мало делал;'
-	
+    'GRIMM - дизайн уровней;' ..
+    '\nThe Epic Prophet - композиционное наполнение, саундтрек (Покойся с миром, бро);' ..
+    '\nMadeInCCCP - композиционное наполнение (https://www.youtube.com/@MaDeInCCCPMUSIC);' ..
+    '\nprioraror - помощь с адаптацией старых работ;' ..
+    '\nDeviatt - много говорил, мало делал;'
+    
 LANG['#Credits_Also_Title'] = 'Создано при материальной поддержке'
 LANG['#Credits_Also_Desc'] = 
-	'Rotatick;'
+    'Rotatick;'
 
 LANG['#Credits_Translators'] = 'Помощь с переводом'
 LANG['#Credits_Translators_Desc'] =
-	'This place is reserved for you, my friend;'
+    'Это место зарезервировано для вас, мой друг;'
 
 LANG['#Press'] = 'Нажмите '
 
@@ -59,33 +59,33 @@ LANG['#MMDisconnect'] = 'Отключиться'
 LANG['#NetworkLoading'] = 'Загрузка сетевых данных'
 LANG['#LoadingMod'] = 'Загрузка модулей:'
 LANG['#LoadingImage'] = 'Загрузка текстур:'
-LANG['#LoadingUI'] = 'Загрузка пользовательских интерфейсов:'
+LANG['#LoadingUI'] = 'Загрузка пользовательского интерфейса:'
 
-LANG['#HUDOptions'] = 'Экранный интерфейс'
+LANG['#HUDOptions'] = 'Настройки HUD'
 LANG['#HUDWeaponNotations'] = 'Инструкция к оружию'
-LANG['#HUDWeaponNotations_Help'] = 'Отображать инструкцию к используемому оружию.'
+LANG['#HUDWeaponNotations_Help'] = 'Отображать инструкцию по использованию оружия.'
 
 LANG['#SoundOptions'] = 'Звук'
-LANG['#CombatMusicVolume'] = 'Громкость музыки в бою'
-LANG['#CombatMusicVolume_Help'] = 'Громкость боевой музыки. Значение "0%" отключит скрипт.'
+LANG['#CombatMusicVolume'] = 'Громкость боевой музыки'
+LANG['#CombatMusicVolume_Help'] = 'Громкость боевой музыки. Значение "0%" отключит музыку.'
 LANG['#SFXVolume'] = 'Громкость звуковых эффектов'
-LANG['#SFXVolume_Help'] = 'Громкость звуковых эффектов и звуков окружение.'
+LANG['#SFXVolume_Help'] = 'Громкость звуковых эффектов и звуков окружения.'
 
 LANG['#PerformanceOptions'] = 'Производительность'
 LANG['#UnloadOption'] = 'Утилизация контента'
 LANG['#UnloadOption_Help'] = 'Удаление неиспользуемого контента из памяти при выходе с сервера.'
 LANG['#ClearTempFilesOption'] = 'Удаление временных файлов'
-LANG['#ClearTempFilesOption_Help'] = 'Удаление временных файлов создаваемых игрой из физической памяти.'
-LANG['#MultithreadOption'] = 'Альтернативный поток рендера'
-LANG['#MultithreadOption_Help'] = 'Обработка запросов отрисовки в альтернативном потоке. Многоядерность!'
+LANG['#ClearTempFilesOption_Help'] = 'Удаление временных файлов, создаваемых игрой, из физической памяти.'
+LANG['#MultithreadOption'] = 'Альтернативный поток рендеринга'
+LANG['#MultithreadOption_Help'] = 'Обработка запросов отрисовки в альтернативном потоке. Использование многоядерности!'
 LANG['#HintExperimentalOption'] = 'Экспериментальная опция!'
 LANG['#3DSky'] = 'Трёхмерный скайбокс'
-LANG['#3DSky_Help'] = 'Активирует рендеринг трёхмерных скайбоксов'
+LANG['#3DSky_Help'] = 'Активирует рендеринг трёхмерных скайбоксов.'
 LANG['#FlushLOD'] = 'Перезагрузить уровни детализации'
 LANG['#FlushLOD_Label'] = 'Перезагрузить'
 LANG['#AsyncSound'] = 'Асинхронный звук'
-LANG['#AsyncSound_Help'] = 'Асинхронная обработка звука. Звуки не воспроизводятся до тех пор, пока данные не поступят в ОЗУ.'
+LANG['#AsyncSound_Help'] = 'Асинхронная обработка звука. Звуки не воспроизводятся, пока данные не поступят в ОЗУ.'
 LANG['#SoundCull'] = 'Выбраковка звука'
-LANG['#SoundCull_Help'] = 'Ограничения дублирующихся звуков в процессе микширования. Значение "0" - отключит опцию.'
+LANG['#SoundCull_Help'] = 'Ограничение дублирующихся звуков в процессе микширования. Значение "0" отключит опцию.'
 LANG['#SoundEnhanced'] = 'Улучшенное стерео'
-LANG['#SoundEnhanced_Help'] = 'Улучшенное микширования звука.'
+LANG['#SoundEnhanced_Help'] = 'Улучшенное микширование звука.'
